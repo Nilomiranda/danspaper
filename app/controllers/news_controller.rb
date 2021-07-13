7aeeb3713e689863fe46aced33cbaaebeed1263c
Nilomiranda <nilomiranda3@gmail.com>
2021-07-13 17:03:49 -0300

    fixed method name required for authentication

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :delete]
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
 
   def index
 
