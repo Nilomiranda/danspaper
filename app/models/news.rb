97388f7849100dce15b079b81802b14b68d5d89f
Nilomiranda <nilomiranda3@gmail.com>
2021-07-12 21:03:27 -0300

    added human friendly slugs to publications urls

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,18 @@
 class News < ApplicationRecord
+  after_validation :set_slug, only: [:create, :update]
+
   has_one_attached :thumbnail
 
   validates_presence_of :content, message: "You can't create a publication without any content"
   validates_presence_of :title, message: "Title is required"
+
+  private
+
+  def set_slug
+    self.slug = title.to_s.parameterize
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 end
